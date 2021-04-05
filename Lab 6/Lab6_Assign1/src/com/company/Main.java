aeadab9e892d83481b4e74fbcafccf4e774da1f2
shahzebkaz <80219157+shahzebkaz@users.noreply.github.com>
2021-04-05 23:49:38 +0500

    Add files via upload

---
+++ Lab 6/Lab6_Assign1/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator.Sum(2, 5);
+        Calculator.Multiply(23,15);
+        Calculator.Modulus(15,18);
+        Calculator.Divide(40,10);
+        Calculator.Sin(30);
+        Calculator.Cos(45);
+        Calculator.Tan(90);
+
+
+    }
+}
